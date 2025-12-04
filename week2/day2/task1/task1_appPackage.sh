cff7e94dc1b09e9f311cceca54f91b0aae733cd5
Dipesh <deepeshpixar123@gmail.com>
2025-12-04 14:37:47 +0545

    Some rechanges were made

--- week2/day2/task1/task1_appPackage.sh
+++ week2/day2/task1/task1_appPackage.sh
@@ -1,45 +1,57 @@
 #!/bin/bash
 
+##### Project variables #####
+PROJECT_NAME="test-app"
+SRC_DIR="$PROJECT_NAME/src"
+PUBLIC_DIR="$PROJECT_NAME/public"
+TARBALL="$PROJECT_NAME-$(date '+%Y%m%d_%H%M%S').tar.gz"
+CHECKSUM_FILE="$TARBALL.sha256sum"
+LOG_FILE="log_report-$PROJECT_NAME-$(date '+%Y%m%d_%H%M%S').txt"
+
 ##### Log report function #####
-report="log_report-$(date '+%Y%m%d_%H%M%S').txt"
 log_report() {
-    echo "$1" | tee -a "$report"                   
+    echo "$1" | tee -a "$LOG_FILE"
 }
 
-##### Files to check for in a React app #####
+##### Files to check for in the project #####
 files=(
-    "test-app/package.json"
-    "test-app/src/index.js"
-    "test-app/src/App.js"
-    "test-app/public/index.html"
+    "$PROJECT_NAME/package.json"
+    "$SRC_DIR/index.js"
+    "$SRC_DIR/App.js"
+    "$PUBLIC_DIR/index.html"
 )
 
+##### Check all required files #####
 app_files_complete="YES"
 for f in "${files[@]}"; do
     if [ ! -e "$f" ]; then
         app_files_complete="NO"
         log_report "Missing required file: $f"
-        exit 1
     fi
 done
 
-##### Folders to exclude #####
-app_folders_to_exclude="test-app/node_modules"
+if [ "$app_files_complete" = "NO" ]; then
+    log_report "One or more required files are missing. Exiting."
+    exit 1
+fi
 
-##### Check existence of complete test-app and create the tarball #####
-if [ -d "test-app" ] && [ "$app_files_complete" = "YES" ]; then
-    tar -czvf test-app.tar.gz --exclude="$app_folders_to_exclude" test-app/
-    log_report "test-app has been zipped into test-app.tar.gz"
+##### Folders to exclude from tarball #####
+app_folders_to_exclude="$PROJECT_NAME/node_modules"
+
+##### Create tarball if project directory exists #####
+if [ -d "$PROJECT_NAME" ]; then
+    tar -czvf "$TARBALL" --exclude="$app_folders_to_exclude" "$PROJECT_NAME"/
+    log_report "$PROJECT_NAME has been zipped into $TARBALL"
 else
-    echo "No test-app to zip or required files incomplete in test-app"
-    log_report "No test-app to zip or required files incomplete in test-app"
+    log_report "Directory '$PROJECT_NAME' does not exist. Exiting."
     exit 1
 fi
 
 ##### Create a checksum of the tarball #####
-sha256sum test-app.tar.gz > test-app.tar.gz.sha256sum   
-log_report "Checksum has been created for the tarball"
+sha256sum "$TARBALL" > "$CHECKSUM_FILE"
+log_report "Checksum file created at $(pwd)/$CHECKSUM_FILE"
 
 ##### Output the path of the tarball and checksum #####
 echo "The path of the packages is $(pwd)"
 log_report "The path of the packages is $(pwd)"
+
