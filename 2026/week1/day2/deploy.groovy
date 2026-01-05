5d2870d2f204beb7110b07157c0e3c4aab57ca28
Dipesh <deepeshpixar123@gmail.com>
2026-01-05 08:49:19 +0545

    create a groovy file for the cicd pipeline

---
+++ 2026/week1/day2/deploy.groovy
@@ -0,0 +1,48 @@
+pipeline{
+  agent any
+  
+  environment{
+    SSH_KEY64= credentials('SSH_KEY64')
+  }
+  parameters {
+      string(
+          name: 'SERVER_IP',
+          defaultValue: '100.31.95.2',
+          description: "Enter the server IP ADDRESS"
+      )
+  }
+
+  stages{
+    stage('Configure SSH'){
+      steps{
+        sh '''
+          mkdir -p ~/.ssh
+          chmod 700 ~/.ssh
+          echo -e "Host *\\n\\tStrictHostKeyChecking no\\n\\n" > ~/.ssh/config
+          cat ~/.ssh/config
+          touch ~/.ssh/known_hosts
+          chmod 600 ~/.ssh/known_hosts
+        '''
+      }
+    }
+    stage('SSH Key Access'){
+      steps{
+        sh 
+        '''
+          touch myKey.pem 
+          echo $SSH_KEY64 | base64 -d> myKey.pem 
+          chmod 400 myKey.pem
+          ssh-keygen -R ${params.SERVER_IP}
+        '''
+      }
+    }
+    stage('Deploy Code to Server'){
+      steps{
+        sh
+        '''
+          ssh -t ec2-user@$${params.SERVER_IP} -i myKey.pem 'cd /usr/share/n ginx/html && git pull origin main'
+        '''
+      }
+    }
+  }
+}
